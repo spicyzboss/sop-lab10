ea9005356bec853d6cb445bf3d64f5f20f4eff84
spicyzboss <73651621+spicyzboss@users.noreply.github.com>
2022-10-11 11:09:42 +0700

    finish

---
+++ src/main/java/com/example/lab09/rest/CreateProductRestModel.java
@@ -0,0 +1,12 @@
+package com.example.lab09.rest;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class CreateProductRestModel {
+    private String title;
+    private BigDecimal price;
+    private Integer quantity;
+}
